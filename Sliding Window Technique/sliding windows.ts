073e9b74d151b503aae6158d6e73fe498349e2df
mdnaimur <mdnaimurr@gmail.com>
2025-03-20 07:43:26 +0600

    algorithm and Data Structrue

---
+++ Sliding Window Technique/sliding windows.ts
@@ -0,0 +1,23 @@
+function maxSubArraySum(nums: number[], k: number): number {
+    let maxSum = Number.MIN_SAFE_INTEGER;
+    let windowSum = 0;
+
+    for (let i = 0; i < k; i++) {
+        windowSum += nums[i];
+    }
+
+    maxSum = windowSum;
+
+    for (let i = k; i < nums.length; i++) {
+        windowSum += nums[i] - nums[i - k];
+        maxSum = Math.max(maxSum, windowSum);
+    }
+
+    return maxSum;
+}
+
+const nums: number[] = [2, 1, 5, 1, 3, 2];
+const windowSize: number = 3;
+const result: number = maxSubArraySum(nums, windowSize);
+
+console.log("Maximum subarray sum:", result);
